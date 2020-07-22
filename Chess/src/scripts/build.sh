572de7372bd2b90bd88d390681c323b1e36fd8f4
Arian Omidi <arian.omidi@icloud.com>
2020-07-21 22:03:54 -0600

    Added build script

---
+++ Chess/src/scripts/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+build(){
+	curdir=`pwd`
+
+	cd /Users/arianomidi/Dropbox/Personal/Code/Java/ChessApplication/Chess
+	
+	#if [[ -d build ]]; then
+	#	rm -r build
+	#fi
+	
+	cp -r src/main/java/ build/
+	
+	cd build
+	javac ChessApplication.java
+
+	cd $curdir
+}
+
+chess(){
+	curdir=`pwd`
+
+	cd /Users/arianomidi/Dropbox/Personal/Code/Java/ChessApplication/Chess/build
+
+	java ChessApplication
+	
+	cd $curdir
+}
+
+#build
